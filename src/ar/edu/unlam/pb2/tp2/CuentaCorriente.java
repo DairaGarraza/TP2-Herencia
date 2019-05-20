7905542a0d7bd89dc394b49d5801896cde97aa93
learanda <learanda@hotmail.com.ar>
2019-05-19 23:54:28 -0300

    Segundo commit de Lean

--- src/ar/edu/unlam/pb2/tp2/CuentaCorriente.java
+++ src/ar/edu/unlam/pb2/tp2/CuentaCorriente.java
@@ -1,11 +1,5 @@
 package ar.edu.unlam.pb2.tp2;
 
-public class CuentaCorriente extends CuentaSueldo {
-
-	public CuentaCorriente(Double saldo) {
-		super(saldo);
-		// TODO Auto-generated constructor stub
-	}
 /*- Permite establecer una cantidad de dinero a girar en descubierto. 
  * 
  *- Cada vez que se desee extraer dinero, no sólo se considera el que se posee, 
@@ -19,6 +13,42 @@ public class CuentaCorriente extends CuentaSueldo {
  *Pasaremos a deberle al banco $ 105 en total: los $ 100 que nos cubrió,
  * más el 5% adicional sobre el descubierto otorgado.
  * */
+
+public class CuentaCorriente extends CuentaSueldo {
+	
+	private Double descubierto;
+	private Double aplicarRecargoAMonto;
+	private Double saldoAnterior;
+	private Double porcentajeDeRecargo;
+
+	public CuentaCorriente(Double saldo) {
+		super(saldo);
+		this.saldoAnterior = 0.0;
+		this.aplicarRecargoAMonto = 0.0;
+		this.porcentajeDeRecargo = 0.0;
+	}
+	
+	@Override
+	public void extraer(Double extraccion) {
+		
+		if(extraccion <= saldo) {
+			this.saldo = saldo - extraccion;
+		}
+		else if(extraccion > this.saldo && this.saldo > 0) {
+			this.saldo = saldo - extraccion;
+			this.aplicarRecargoAMonto = this.saldo;
+			this.porcentajeDeRecargo = this.aplicarRecargoAMonto*0.5;
+			this.saldo = this.saldo + this.porcentajeDeRecargo;
+		}
+		else if(extraccion > this.saldo && this.saldo < 0) {
+			this.saldoAnterior = this.saldo;
+			this.saldo = saldo - extraccion;
+			this.aplicarRecargoAMonto = this.saldo - this.saldoAnterior;
+			this.porcentajeDeRecargo = this.aplicarRecargoAMonto*0.5;
+			this.saldo = this.saldo + this.porcentajeDeRecargo;
+		}
+	}
+
 	
 	
 }
